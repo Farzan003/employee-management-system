fdbd4269c39650cf7c82f4695fef8ec35280481d
Farzan003 <161925914+Farzan003@users.noreply.github.com>
2024-05-15 20:36:45 +0500

    Update EMPLOYEES MANAGEMENT SYSTEM.sql

--- EMPLOYEES MANAGEMENT SYSTEM.sql
+++ EMPLOYEES MANAGEMENT SYSTEM.sql
@@ -2,7 +2,7 @@ create database employeemanagementsystem;
 
 use employeemanagementsystem;
 
-//TABLE 1 EMPLOYEES
+-- TABLE 1 EMPLOYEES
 
 create table Employees(
 EMPLOYEE_ID INT PRIMARY KEY,
@@ -13,14 +13,14 @@ POSITION VARCHAR(50),
 SALARY DECIMAL (10,2)
 );
 
-// TABLE2 TO STORE DEPARTMENT INFORMATION
+-- TABLE2 TO STORE DEPARTMENT INFORMATION
 
 CREATE TABLE DEPARTMENTS(
 DEPARTMENT_ID INT PRIMARY KEY,
 DEPARTMENT_NAME VARCHAR(50)
 );
 
-//TABLE 3 TO STORE EMPLOYEES ADRESSES
+-- TABLE 3 TO STORE EMPLOYEES ADRESSES
 
 CREATE TABLE EMPLOYEES_ADRESSES(
 ADRESS_ID INT,
@@ -29,7 +29,7 @@ CITY VARCHAR(50),
 PROVINCE VARCHAR(50)
 );
 
-// Table4 to store employee contact information
+-- Table4 to store employee contact information
 
 CREATE TABLE EmployeeContacts(
     ContactID INT PRIMARY KEY,
@@ -38,7 +38,7 @@ CREATE TABLE EmployeeContacts(
     Phone VARCHAR(15)
 );
 
-// Table5 to store employee skills
+-- Table5 to store employee skills
 
 CREATE TABLE EmployeeSkills (
     SkillID INT PRIMARY KEY,
@@ -46,7 +46,7 @@ CREATE TABLE EmployeeSkills (
     SkillName VARCHAR(50)
 );
 
-// Table6 to store employee projects
+-- Table6 to store employee projects
 
 CREATE TABLE EmployeeProjects (
     ProjectID INT PRIMARY KEY,
@@ -56,7 +56,7 @@ CREATE TABLE EmployeeProjects (
     EndDate DATE
 );
 
-// Table7 to store employee performance reviews
+-- Table7 to store employee performance reviews
 
 CREATE TABLE EmployeeReviews (
     ReviewID INT PRIMARY KEY,
@@ -66,7 +66,7 @@ CREATE TABLE EmployeeReviews (
     Comments TEXT
 );
 
-// Table8 to store employee benefits
+-- Table8 to store employee benefits
 
 CREATE TABLE EmployeeBenefits (
     BenefitID INT PRIMARY KEY,
@@ -76,7 +76,7 @@ CREATE TABLE EmployeeBenefits (
     BenefitAmount DECIMAL(10, 2)
 );
 
-// Table9 to store employee attendance
+-- Table9 to store employee attendance
 
 CREATE TABLE EmployeeAttendance (
     AttendanceID INT PRIMARY KEY,
@@ -86,7 +86,7 @@ CREATE TABLE EmployeeAttendance (
     ClockOutTime TIME
 );
 
-// Table10 to store employee training
+-- Table10 to store employee training
 
 CREATE TABLE EmployeeTraining (
     TrainingID INT PRIMARY KEY,
